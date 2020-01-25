ecd592ec928e41544b38de7610f9f578979f45d9
cdthomp1 <tho17015@byui.edu>
2020-01-25 13:09:34 -0700

    changes

---
+++ team-activities/week-02/script.js
@@ -0,0 +1,7 @@
+function alertUser(id){ 
+    alert("The " + id + " has been clicked");
+}
+
+function changeColor() {
+    document.getElementById("div1").style.backgroundColor = document.getElementById("colorField").value;
+}
