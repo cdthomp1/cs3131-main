ec1e3b1951729536504fa8d1aa4c67ca45dac5d6
cdthomp1 <tho17015@byui.edu>
2020-01-25 18:57:49 -0700

    new

--- prove/test/cart.php
+++ prove/test/cart.php
@@ -1,8 +1,9 @@
 <div id="shopping-cart">
-        <div class="txt-heading">Shopping Cart <a id="btnEmpty" class="cart-action" onClick="cartAction('empty','');"><img src="images/icon-empty.png" /> Empty Cart</a></div>
-        <div id="cart-item">
-            <?php
-            require_once "ajax-action.php";
-            ?>
-        </div>
+    <div class="txt-heading">Shopping Cart <a id="btnEmpty" class="cart-action" onClick="cartAction('empty','');"><img src="images/icon-empty.png" /> Empty Cart</a></div>
+    <div id="cart-item">
+        <?php
+        require_once "ajax-action.php";
+        ?>
     </div>
+</div>
+<script src="./home/prove/test/cartAction.js"></script>
--- prove/test/index.php
+++ prove/test/index.php
@@ -20,39 +20,7 @@ session_start();
   src="https://code.jquery.com/jquery-3.4.1.min.js"
   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
   crossorigin="anonymous"></script>
-    <script>
-        function cartAction(action, product_code) {
-            var queryString = "";
-            if (action != "") {
-                switch (action) {
-                    case "add":
-                        queryString = 'action=' + action + '&code=' + product_code +
-                            '&quantity=' + $("#qty_" + product_code).val();
-                        break;
-                    case "remove":
-                        queryString = 'action=' + action + '&code=' + product_code;
-                        break;
-                    case "empty":
-                        queryString = 'action=' + action;
-                        break;
-                }
-            }
-            jQuery.ajax({
-                url: "ajax-action.php",
-                data: queryString,
-                type: "POST",
-                success: function(data) {
-                    $("#cart-item").html(data);
-                    if (action == "add") {
-                        $("#add_" + product_code + " img").attr("src",
-                            "images/icon-check.png");
-                        $("#add_" + product_code).attr("onclick", "");
-                    }
-                },
-                error: function() {}
-            });
-        }
-    </script>
+    <script src="./home/prove/test/cartAction.js"></script>
 </body>
 
 </html>
