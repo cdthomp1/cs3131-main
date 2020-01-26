8d2c3554ab72d79ed019773aa7d1aae6418b9cb9
cdthomp1 <tho17015@byui.edu>
2020-01-25 19:49:18 -0700

    new style

--- prove/test/product-gallery.php
+++ prove/test/product-gallery.php
@@ -11,18 +11,33 @@ $productArray = $product->getAllProduct();
     ?>
             <div class="product">
                 <form id="frmCart">
+                    <div class="mos9">
+                        <div class="mos9-window">
+                            <div class="mos9-window__header">
+                                <div class="mos9-window__tools">
+                                    <a href="#" class="mos9-window__close mos9-button"></a>
+                                </div>
+                                <div class="mos9-window__title"><?php echo $productArray[$k]["name"]; ?></div>
+                                <div class="mos9-window__tools">
+                                    <a href="#" class="mos9-window__tool--1 mos9-button"></a>
+                                    <a href="#" class="mos9-window__tool--2 mos9-button"></a>
+                                </div>
+                                <div class="mos9-window__content">
 
-                    <p><?php echo $productArray[$k]["name"]; ?></p>
-                    <img src="<?php echo $productArray[$k]["image"]; ?>">
+                                    <img src="<?php echo $productArray[$k]["image"]; ?>">
 
 
 
-                    <p><?php echo "$" . $productArray[$k]["price"]; ?></p>
+                                    <p><?php echo "$" . $productArray[$k]["price"]; ?></p>
 
-                    <button type="button" id="add_<?php echo $productArray[$k]["code"]; ?>" class="btnAddAction cart-action" onClick="cartAction('add','<?php echo $productArray[$k]["code"]; ?>')">
-                        <img src="images/add-to-cart.png" />
-                    </button>
-                    <input type="text" id="qty_<?php echo $productArray[$k]["code"]; ?>" name="quantity" value="1" size="2" />
+                                    <button type="button" id="add_<?php echo $productArray[$k]["code"]; ?>" class="btnAddAction cart-action" onClick="cartAction('add','<?php echo $productArray[$k]["code"]; ?>')">
+                                        <img src="images/add-to-cart.png" />
+                                    </button>
+                                    <input type="text" id="qty_<?php echo $productArray[$k]["code"]; ?>" name="quantity" value="1" size="2" />
+
+                                </div>
+                            </div>
+                        </div>
 
                 </form>
             </div>
