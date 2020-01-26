4f6b164abd7c963f82458dbdcb25ae6e8a70839b
cdthomp1 <tho17015@byui.edu>
2020-01-25 19:35:13 -0700

    new style

--- prove/test/product-gallery.php
+++ prove/test/product-gallery.php
@@ -1,40 +1,34 @@
 <?php
-require_once ("Product.php");
+require_once("Product.php");
 $product = new Product();
 $productArray = $product->getAllProduct();
 ?>
-<div id="product-grid">
-    <div class="txt-heading">Products</div>
-<?php
-if (! empty($productArray)) {
-    foreach ($productArray as $k => $v) {
-        ?>
-		<div class="product-item">
-        <form id="frmCart">
-            <div class="product-images">
-                <img class="product-image" src="<?php echo $productArray[$k]["image"]; ?>">
-            </div>
-            <div>
-                <div class="product-info">
-                    <strong><?php echo $productArray[$k]["name"]; ?></strong>
-                </div>
-                <div class="product-info product-price"><?php echo "$".$productArray[$k]["price"]; ?></div>
-                <div class="product-info">
-                    <button type="button"
-                        id="add_<?php echo $productArray[$k]["code"]; ?>"
-                        class="btnAddAction cart-action"
-                        onClick="cartAction('add','<?php echo $productArray[$k]["code"]; ?>')">
+
+<div class="txt-heading">Products</div>
+<div class="productContainer">
+    <?php
+    if (!empty($productArray)) {
+        foreach ($productArray as $k => $v) {
+    ?>
+            <div class="product">
+                <form id="frmCart">
+
+                    <p><?php echo $productArray[$k]["name"]; ?></p>
+                    <img src="<?php echo $productArray[$k]["image"]; ?>">
+
+
+
+                    <p><?php echo "$" . $productArray[$k]["price"]; ?></p>
+
+                    <button type="button" id="add_<?php echo $productArray[$k]["code"]; ?>" class="btnAddAction cart-action" onClick="cartAction('add','<?php echo $productArray[$k]["code"]; ?>')">
                         <img src="images/add-to-cart.png" />
                     </button>
-                    <input type="text"
-                        id="qty_<?php echo $productArray[$k]["code"]; ?>"
-                        name="quantity" value="1" size="2" />
-                </div>
+                    <input type="text" id="qty_<?php echo $productArray[$k]["code"]; ?>" name="quantity" value="1" size="2" />
+
+                </form>
             </div>
-        </form>
-    </div>
-	<?php
-    }
-}
-?>
 </div>
+<?php
+        }
+    }
+?>
