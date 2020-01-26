40fa1112f9a9b24b26386675d60296195124708b
cdthomp1 <tho17015@byui.edu>
2020-01-25 19:58:32 -0700

    what

--- prove/test/product-gallery.php
+++ prove/test/product-gallery.php
@@ -9,38 +9,31 @@ $productArray = $product->getAllProduct();
     if (!empty($productArray)) {
         foreach ($productArray as $k => $v) {
     ?>
-
-            <form id="frmCart">
-                <div class="mos9">
-                    <div class="mos9-window">
-                        <div class="mos9-window__header">
-                            <div class="mos9-window__tools">
-                                <a href="#" class="mos9-window__close mos9-button"></a>
-                            </div>
-                            <div class="mos9-window__title"><?php echo $productArray[$k]["name"]; ?></div>
-                            <div class="mos9-window__tools">
-                                <a href="#" class="mos9-window__tool--1 mos9-button"></a>
-                                <a href="#" class="mos9-window__tool--2 mos9-button"></a>
-                            </div>
-                            <div class="mos9-window__content">
-
-                                <img src="<?php echo $productArray[$k]["image"]; ?>">
-
-
-
-                                <p><?php echo "$" . $productArray[$k]["price"]; ?></p>
-
-                                <button type="button" id="add_<?php echo $productArray[$k]["code"]; ?>" class="btnAddAction cart-action" onClick="cartAction('add','<?php echo $productArray[$k]["code"]; ?>')">
-                                    <img src="images/add-to-cart.png" />
-                                </button>
-                                <input type="text" id="qty_<?php echo $productArray[$k]["code"]; ?>" name="quantity" value="1" size="2" />
-
+            <div class="product">
+                <form id="frmCart">
+                    <div class="mos9">
+                        <div class="mos9-window">
+                            <div class="mos9-window__header">
+                                <div class="mos9-window__tools">
+                                    <a href="#" class="mos9-window__close mos9-button"></a>
+                                </div>
+                                <div class="mos9-window__title"><?php echo $productArray[$k]["name"]; ?></div>
+                                <div class="mos9-window__tools">
+                                    <a href="#" class="mos9-window__tool--1 mos9-button"></a>
+                                    <a href="#" class="mos9-window__tool--2 mos9-button"></a>
+                                </div>
+                                <div class="mos9-window__content">
+                                    <img src="<?php echo $productArray[$k]["image"]; ?>">
+                                    <p><?php echo "$" . $productArray[$k]["price"]; ?></p>
+                                    <button type="button" id="add_<?php echo $productArray[$k]["code"]; ?>" class="btnAddAction cart-action" onClick="cartAction('add','<?php echo $productArray[$k]["code"]; ?>')">
+                                        <img src="images/add-to-cart.png" />
+                                    </button>
+                                    <input type="text" id="qty_<?php echo $productArray[$k]["code"]; ?>" name="quantity" value="1" size="2" />
+                                </div>
                             </div>
                         </div>
-                    </div>
-
-            </form>
-
+                </form>
+            </div>
     <?php
         }
     }
@@ -65,4 +58,5 @@ $productArray = $product->getAllProduct();
             </div>
         </div>
     </div>
+
 </div>
