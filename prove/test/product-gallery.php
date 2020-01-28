5bdc1a513f6faea83051356170b89f6ac327d68f
cdthomp1 <tho17015@byui.edu>
2020-01-27 22:54:17 -0700

    one step at a time

--- prove/test/product-gallery.php
+++ prove/test/product-gallery.php
@@ -1,40 +1,43 @@
 <?php
-require_once ("Product.php");
+require_once("Product.php");
 $product = new Product();
 $productArray = $product->getAllProduct();
 ?>
-<div id="product-grid">
-    <div class="txt-heading">Products</div>
-<?php
-if (! empty($productArray)) {
-    foreach ($productArray as $k => $v) {
-        ?>
-		<div class="product-item">
-        <form id="frmCart">
-            <div class="product-image">
-                <img src="<?php echo $productArray[$k]["image"]; ?>">
-            </div>
-            <div>
-                <div class="product-info">
-                    <strong><?php echo $productArray[$k]["name"]; ?></strong>
+
+<div class="productContainer">
+    <?php
+    if (!empty($productArray)) {
+        foreach ($productArray as $k => $v) {
+    ?>
+            <div class="product">
+                <div class="mos9">
+                    <div class="mos9-window">
+                        <div class="mos9-window__header">
+                            <div class="mos9-window__tools">
+                                <a href="#" class="mos9-window__close mos9-button"></a>
+                            </div>
+                            <div class="mos9-window__title"><?php echo $productArray[$k]["name"]; ?></div>
+                            <div class="mos9-window__tools">
+                                <a href="#" class="mos9-window__tool--1 mos9-button"></a>
+                                <a href="#" class="mos9-window__tool--2 mos9-button"></a>
+                            </div>
+                        </div>
+                        <div class="mos9-window__content">
+                            <img src="<?php echo $productArray[$k]["image"]; ?>">
+                            <p><?php echo "$" . $productArray[$k]["price"]; ?></p>
+                            <div class="mos9-window__content-control">
+                                <button type="button" id="add_<?php echo $productArray[$k]["code"]; ?>" class="btnAddAction cart-action" onClick="cartAction('add','<?php echo $productArray[$k]["code"]; ?>')">
+                                    <img src="images/add-to-cart.png" />
+                                </button>
+                                <input type="text" id="qty_<?php echo $productArray[$k]["code"]; ?>" name="quantity" value="1" size="2" />
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="product-info product-price"><?php echo "$".$productArray[$k]["price"]; ?></div>
-                <div class="product-info">
-                    <button type="button"
-                        id="add_<?php echo $productArray[$k]["code"]; ?>"
-                        class="btnAddAction cart-action"
-                        onClick="cartAction('add','<?php echo $productArray[$k]["code"]; ?>')">
-                        <img src="images/add-to-cart.png" />
-                    </button>
-                    <input type="text"
-                        id="qty_<?php echo $productArray[$k]["code"]; ?>"
-                        name="quantity" value="1" size="2" />
-                </div>
             </div>
-        </form>
-    </div>
-	<?php
+    <?php
+        }
     }
-}
-?>
+    ?>
+
 </div>
