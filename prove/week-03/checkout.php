accc56a26563c7f499fcd07df2765a44fea2f1e8
cdthomp1 <tho17015@byui.edu>
2020-01-31 01:47:10 -0700

    asdas

--- prove/week-03/checkout.php
+++ prove/week-03/checkout.php
@@ -29,19 +29,21 @@ $_SESSION['fname'] = $_POST['fname'];
                     </div>
                     <div class="mos9-window__content">
                         <div id="checkout-form">
-                            <label for="fname"><i class="fa fa-user"></i> Full Name</label>
-                            <input type="text" id="fname" name="firstname" placeholder="John M. Doe">
-                            <label for="email"><i class="fa fa-envelope"></i> Email</label>
-                            <input type="text" id="email" name="email" placeholder="john@example.com">
-                            <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
-                            <input type="text" id="adr" name="address" placeholder="542 W. 15th Street">
-                            <label for="city"><i class="fa fa-institution"></i> City</label>
-                            <input type="text" id="city" name="city" placeholder="New York">
-                            <label for="state">State</label>
-                            <input type="text" id="state" name="state" placeholder="NY">
-                            <label for="zip">Zip</label>
-                            <input type="text" id="zip" name="zip" placeholder="10001">
-                            <button type='submit' name='psubmit' value='SUBMIT'>Add Address</button>
+                            <form method="post">
+                                <label for="fname"><i class="fa fa-user"></i> Full Name</label>
+                                <input type="text" id="fname" name="firstname" placeholder="John M. Doe">
+                                <label for="email"><i class="fa fa-envelope"></i> Email</label>
+                                <input type="text" id="email" name="email" placeholder="john@example.com">
+                                <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
+                                <input type="text" id="adr" name="address" placeholder="542 W. 15th Street">
+                                <label for="city"><i class="fa fa-institution"></i> City</label>
+                                <input type="text" id="city" name="city" placeholder="New York">
+                                <label for="state">State</label>
+                                <input type="text" id="state" name="state" placeholder="NY">
+                                <label for="zip">Zip</label>
+                                <input type="text" id="zip" name="zip" placeholder="10001">
+                                <button type='submit' name='psubmit' value='SUBMIT'>Add Address</button>
+                            </form>
                         </div>
                     </div>
                 </div>
