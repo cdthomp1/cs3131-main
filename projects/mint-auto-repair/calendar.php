7186a71fbbf2e10f4bed539fc161f2e96aa9980d
cdthomp1 <tho17015@byui.edu>
2020-02-13 23:49:45 -0700

    test

--- projects/mint-auto-repair/calendar.php
+++ projects/mint-auto-repair/calendar.php
@@ -40,13 +40,16 @@ try {
             <label for="date">You would like to create an appointment on: </label><input id="date" type="text" name="date"><br />
             <label for="name">What is your name? </label><input id="name" type="text" name="name"></br />
             <label for="time">What time?</label><input id="time" type="time" name="time"><br />
-            <label for="tech">Who would you like to work on your car? </label><select id="tech" name="tech"><?php
-foreach ($db->query("SELECT * FROM employees WHERE employee_position='technician'") as $row) {
-    echo '<option value="'.$row['employee_name'].'"><'. $row['employee_name'].'</option>';
-}
-?>
+            <label for="tech">Who would you like to work on your car? </label><select id="tech" name="tech">
             </select>
         </form>
+
+        <?php
+foreach ($db->query("SELECT * FROM employees WHERE employee_position='technician'") as $row) {
+    //echo '<option value="'.$row['employee_name'].'"><'. $row['employee_name'].'</option>';
+    echo $row['employee_name'];
+}
+?>
     </div>
     <script src="schedule.js"></script>
 </body>
