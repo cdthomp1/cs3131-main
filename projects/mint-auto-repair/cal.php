4942452c500b17a1ea192fc41fe3b130906ffd01
cdthomp1 <tho17015@byui.edu>
2020-02-13 22:52:27 -0700

    test

--- projects/mint-auto-repair/cal.php
+++ projects/mint-auto-repair/cal.php
@@ -121,7 +121,7 @@ class Calendar {
         }
              
          
-        return '<li id="li-'.$this->currentDate.'" class="'.($cellNumber%7==1?' start ':($cellNumber%7==0?' end ':' ')).
+        return '<li id="li-'.$this->currentDate.'" onclick="alert(this.id)" class="'.($cellNumber%7==1?' start ':($cellNumber%7==0?' end ':' ')).
                 ($cellContent==null?'mask':'').'">'.$cellContent.'</li>';
     }
      
