c1199eec4f220f98da1969a52683c2b5729201bf
cdthomp1 <tho17015@byui.edu>
2020-02-14 00:07:36 -0700

    change

--- projects/mint-auto-repair/details.php
+++ projects/mint-auto-repair/details.php
@@ -8,8 +8,17 @@
 <body>
 <?php
   $date = $_GET["date"];
+  $name = $_GET["name"];
+  $time = $_GET["time"];
+  $tech = $_GET["tech"];
+  $typeOf = $_GET["typeOf"];
 
-  echo '<p>'.$date.'</p>';
+
+  echo '<p>DATE: '.$date.'</p>';
+  echo '<p>NAME: '.$name.'</p>';
+  echo '<p>TIME: '.$time.'</p>';
+  echo '<p>TECH: '.$tech.'</p>';
+  echo '<p>APPOINTMENT TYPE: '.$typeOf.'</p>';
 ?>
 </body>
 </html>
