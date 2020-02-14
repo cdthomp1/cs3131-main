5c01b9301103def65000ce49ef47bcb4159c8571
cdthomp1 <tho17015@byui.edu>
2020-02-13 23:04:07 -0700

    test

--- projects/mint-auto-repair/schedule.js
+++ projects/mint-auto-repair/schedule.js
@@ -1,6 +1,11 @@
 function showDate(id) {
+    showForm();
     var dateField = document.getElementById("date");
     var date = id.replace(/([a-z])\w-+/g, "");
     dateField.value = date;
 
+}
+
+function showForm() {
+    document.getElementsByClassName("scheduleForm")[0].display = "block";
 }
