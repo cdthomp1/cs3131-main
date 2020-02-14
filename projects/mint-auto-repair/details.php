2776df8bd61b6ee500aab6b2fcc862f72527b737
cdthomp1 <tho17015@byui.edu>
2020-02-14 00:04:31 -0700

    try

--- projects/mint-auto-repair/details.php
+++ projects/mint-auto-repair/details.php
@@ -1,5 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
   $date = $_GET["date"];
 
-  echo $date;
+  echo '<p>'.$date.'</p>';
 ?>
+</body>
+</html>
+
+
+
