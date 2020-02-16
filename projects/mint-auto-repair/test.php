55c9c39c7e910a5f1f7c3b800ae331f0e56770dc
cdthomp1 <tho17015@byui.edu>
2020-02-15 22:19:02 -0700

    Test

---
+++ projects/mint-auto-repair/test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+?>
+
+<h2>Congradulations <?php $name?></h2>
+<p>You have successfuly created an appointment with <?php echo $tech?> @ <?php echo $time?> on <?php echo $date?> for an <?php echo $typeOf?>!</p>
+</body>
+</html>
+
+
