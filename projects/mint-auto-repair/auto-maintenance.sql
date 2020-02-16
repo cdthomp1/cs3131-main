09828d6bdf672d817ad1a893b347a85624ee22cd
cdthomp1 <tho17015@byui.edu>
2020-02-15 22:23:14 -0700

    ignore me

---
+++ projects/mint-auto-repair/auto-maintenance.sql
@@ -0,0 +1,42 @@
+CREATE TABLE customer (
+    customer_id SERIAL PRIMARY KEY,
+    customer_name varchar(255) NOT NULL UNIQUE,
+    customer_email varchar(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE technician (
+    technician_id SERIAL PRIMARY KEY,
+    technician_name varchar(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE vehicle (
+    vehicle_id SERIAL PRIMARY KEY,
+    vehicle_make varchar(255) NOT NULL UNIQUE,
+    vehicle_model varchar(255) NOT NULL UNIQUE,
+    vehicle_color varchar(255) NOT NULL UNIQUE,
+    vehicle_license_plate varchar(255) NOT NULL UNIQUE,
+    customer_id integer NOT NULL REFERENCES customer (customer_id)
+);
+
+CREATE TABLE notes (
+    note_id SERIAL PRIMARY KEY,
+    note_text varchar(255) NOT NULL UNIQUE,
+    technician_id integer NOT NULL REFERENCES technician (technician_id),
+    vehicle_id integer NOT NULL REFERENCES vehicle (vehicle_id)
+)
+
+CREATE TABLE appointment (
+    appointment_id SERIAL PRIMARY KEY,
+    appointment_date varchar(255) NOT NULL UNIQUE,
+    appointment_time varchar(255) NOT NULL UNIQUE,
+    appointmet_type varchar(255) NOT NULL UNIQUE,
+    appointment_est_cost integer NOT NULL UNIQUE,
+    appointment_vehicle_id integer NOT NULL REFERENCES vehicle (vehicle_id),
+    appointment_customer_id integer NOT NULL REFERENCES customer (customer_id),
+    appointment_remind_next_apt DATE NOT NULL DEFAULT CURRENT_DATE,
+    appointment_working_tech_id integer NOT NULL REFERENCES technician (technician_id)
+);
+
+
+ALTER TABLE appointment  
+   ADD COLUMN appointment_working_tech_id integer NOT NULL REFERENCES employees (employee_id) DEFAULT 9
