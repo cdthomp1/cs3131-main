9b0bd4b2d190fa63efa334e6672e3a051bae4d4e
cdthomp1 <tho17015@byui.edu>
2020-02-16 00:20:42 -0700

    gsfsdf

--- projects/mint-auto-repair/sucessSignIn.php
+++ projects/mint-auto-repair/sucessSignIn.php
@@ -3,6 +3,7 @@
 
 <?php
   $_SESSION["email"] = $_GET["email"];
+  $_SESSION["name"] = $_GET["name"];
   $_SESSION["pass"] = $_GET["psw"];
   $_SESSION["loggedIn"] = true;
 
@@ -12,27 +13,17 @@
     $_SESSION["rememberMe"] = false;
   }
 
+  $userLoginStatement = $db->query('SELECT exists (SELECT 1 FROM customer WHERE customer_email = :email and customer_password = :psw LIMIT 1)');
+  $userLoginStatement->bindParam(':email', $_SESSION["email"]);
+  $userLoginStatement->bindParam(':psw', $_SESSION["pass"]);
+  $userLoginStatement->execute();
 
-  $sql = "SELECT exists (SELECT 1 FROM customer WHERE customer_email = '".$_SESSION["email"]."' and customer_password = '".$_SESSION["pass"]."' LIMIT 1);";
-  echo $sql . "<br>";
 
-  echo $db;
-
-  $ret = pg_query($db, $sql);
-     if(!$ret){
-        echo pg_last_error($db);
-        exit;
-     }
-  $bool = pg_fetch_row($ret);
-  echo $bool['0'] . "<br>";
-  if($bool['0'] === "t"){
-          echo "Login Successful";
-
-  }else{
-          $errors = $errors . "Passwords do not match. Please try again. <br>";
+  if($userLoginStatement->rowCount() > 0) {
+    echo "hello";
+  } else {
+    echo "try again";
   }
-
-
 ?>
 
 
