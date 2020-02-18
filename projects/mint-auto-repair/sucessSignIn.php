ccc2b682688a2224fc92e7f63d48eea46e35bc11
cdthomp1 <tho17015@byui.edu>
2020-02-17 23:04:18 -0700

    asd

--- projects/mint-auto-repair/sucessSignIn.php
+++ projects/mint-auto-repair/sucessSignIn.php
@@ -1,30 +1,48 @@
 <?php session_start(); ?>
-<?php include './dbConnect.php'; ?>
+
 
 <?php
-  $_SESSION["email"] = $_POST["email"];
-  //$_SESSION["name"] = $_GET["name"];
-  $_SESSION["pass"] = $_POST["psw"];
-  $_SESSION["loggedIn"] = true;
+$_SESSION["email"] = $_POST["email"];
+//$_SESSION["name"] = $_GET["name"];
+$_SESSION["pass"] = $_POST["psw"];
+$_SESSION["loggedIn"] = true;
+
+try {
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"], '/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $ex) {
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+$loggedInEmail = $_POST["email"];
+//$_SESSION["name"] = $_GET["name"];
+$loggedInPassword = $_POST["psw"];
 
 
-  $loggedInEmail = $_POST["email"];
-  //$_SESSION["name"] = $_GET["name"];
-  $loggedInPassword = $_POST["psw"];
+$sql = "SELECT * FROM customer WHERE customer_email='" . $loggedInEmail . "' AND customer_password='" . $loggedInPassword . "' LIMIT 1";
 
 
-  $sql = "SELECT * FROM customer WHERE customer_email='". $loggedInEmail . "' AND customer_password='".$loggedInPassword. "' LIMIT 1";
+$res = pg_query($sql);
 
+echo pg_num_rows($res);
 
-  $res = pg_query($sql);
-
-  echo pg_num_rows($res);
-
-  if ($_GET["remember"] == "on") {
-      $_SESSION["rememberMe"] = true;
-  } else {
-    $_SESSION["rememberMe"] = false;
-  }
+if ($_GET["remember"] == "on") {
+  $_SESSION["rememberMe"] = true;
+} else {
+  $_SESSION["rememberMe"] = false;
+}
 
 
 
