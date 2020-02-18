e41bdbebbdd06b90fc257c07da3d5c08a4cc7f3c
cdthomp1 <tho17015@byui.edu>
2020-02-17 23:22:24 -0700

    sdfsf

--- projects/mint-auto-repair/sucessSignIn.php
+++ projects/mint-auto-repair/sucessSignIn.php
@@ -2,39 +2,61 @@
 <?php include './dbConnect.php'; ?>
 
 <?php
-  $_SESSION["email"] = $_POST["email"];
-  //$_SESSION["name"] = $_GET["name"];
-  $_SESSION["pass"] = $_POST["psw"];
-  $_SESSION["loggedIn"] = true;
+$_SESSION["email"] = $_POST["email"];
+//$_SESSION["name"] = $_GET["name"];
+$_SESSION["pass"] = $_POST["psw"];
+$_SESSION["loggedIn"] = true;
 
 
-  $loggedInEmail = $_POST["email"];
-  //$_SESSION["name"] = $_GET["name"];
-  $loggedInPassword = $_POST["psw"];
+$loggedInEmail = $_POST["email"];
+//$_SESSION["name"] = $_GET["name"];
+$loggedInPassword = $_POST["psw"];
 
 
-  $sql = "SELECT * FROM customer WHERE customer_email='". $loggedInEmail . "' AND customer_password='".$loggedInPassword."'";
-
-  echo $sql;
+$sql = "SELECT * FROM customer WHERE customer_email='" . $loggedInEmail . "' AND customer_password='" . $loggedInPassword . "'";
 
 
-  $res = $db->query($sql);
-  $rows = $res->rowCount();
 
-  if ($rows == 1) {
-    echo "welcome" . $loggedInEmail;
-  } else {
-    header("Location: login.php?error=Username%20or%20password%20incorrect");
-  }
+$sth = $db->query($sql);
 
 
-  echo $rows;
+if ($rows == 1) {
+  /* Exercise PDOStatement::fetch styles */
+  print("PDO::FETCH_ASSOC: ");
+  print("Return next row as an array indexed by column name\n");
+  $result = $sth->fetch(PDO::FETCH_ASSOC);
+  print_r($result);
+  print("\n");
 
-  if ($_GET["remember"] == "on") {
-      $_SESSION["rememberMe"] = true;
-  } else {
-    $_SESSION["rememberMe"] = false;
-  }
+  print("PDO::FETCH_BOTH: ");
+  print("Return next row as an array indexed by both column name and number\n");
+  $result = $sth->fetch(PDO::FETCH_BOTH);
+  print_r($result);
+  print("\n");
+
+  print("PDO::FETCH_LAZY: ");
+  print("Return next row as an anonymous object with column names as properties\n");
+  $result = $sth->fetch(PDO::FETCH_LAZY);
+  print_r($result);
+  print("\n");
+
+  print("PDO::FETCH_OBJ: ");
+  print("Return next row as an anonymous object with column names as properties\n");
+  $result = $sth->fetch(PDO::FETCH_OBJ);
+  print $result->name;
+  print("\n");
+} else {
+  header("Location: login.php?error=Username%20or%20password%20incorrect");
+}
+
+
+echo $rows;
+
+if ($_GET["remember"] == "on") {
+  $_SESSION["rememberMe"] = true;
+} else {
+  $_SESSION["rememberMe"] = false;
+}
 
 
 
