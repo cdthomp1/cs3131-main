434f9dd5a374722cd59ed4729b7cfa8a83167ee5
cdthomp1 <tho17015@byui.edu>
2020-02-17 23:52:31 -0700

    js test

--- projects/mint-auto-repair/scripts/schedule.js
+++ projects/mint-auto-repair/scripts/schedule.js
@@ -1,11 +1,20 @@
+var currentID = '';
+var pastID = '';
+
 function showDate(id) {
-    showForm();
+    changeColor(id);
     var dateField = document.getElementById("date");
     var date = id.replace(/([a-z])\w-+/g, "");
     dateField.value = date;
 
 }
 
-function showForm() {
-    document.getElementsByClassName("scheduleForm")[0].style.display = "block";
+function changeColor(id) {
+    currentID = id;
+    if (currentID !== pastID) {
+        document.getElementById(id).style.backgroundColor = '#42989d';
+    } else {
+        document.getElementById(id).style.backgroundColor = '#dddddd';
+    }
+    pastID = id;
 }
