c3fc61a9ebdbff440cbe717025c7ea967def601d
cdthomp1 <tho17015@byui.edu>
2020-02-20 22:01:16 -0700

    asdfgh

--- projects/mint-auto-repair/profile.php
+++ projects/mint-auto-repair/profile.php
@@ -11,6 +11,7 @@
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <link href="https://fonts.googleapis.com/css?family=Comfortaa:500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles/main.css">
     <link href="styles/profile.css" rel="stylesheet">
 </head>
 
