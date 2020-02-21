0a1fc749091a8dc0802347f1f93f98d0cb73dd8d
cdthomp1 <tho17015@byui.edu>
2020-02-20 22:03:11 -0700

    sdfghj

--- projects/mint-auto-repair/calendar.php
+++ projects/mint-auto-repair/calendar.php
@@ -5,6 +5,7 @@
 
 <head>
     <link href="https://fonts.googleapis.com/css?family=Comfortaa:500&display=swap" rel="stylesheet">
+    
     <link href="styles/calendar.css" type="text/css" rel="stylesheet" />
     <link href="styles/main.css" type="text/css" rel="stylesheet" />
 </head>
