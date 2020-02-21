0f8055ee93ebc560f368346c897c999341bf5531
cdthomp1 <tho17015@byui.edu>
2020-02-21 12:19:16 -0700

    asdfgh

--- projects/mint-auto-repair/calendar.php
+++ projects/mint-auto-repair/calendar.php
@@ -16,7 +16,7 @@
         <div class="scheduleForm">
             <div class="welcome">
                 <h2>Welcome <?php echo $_SESSION["name"] ?><h3>Mint is excited to work with you and your vehicle. Lets begin.</h3>
-                <?php echo $db->query('SELECT appointment_date FROM appointment where appointment_customer_id = 1')?>
+                <?php print_r( $db->query('SELECT appointment_date FROM appointment where appointment_customer_id = 1'))?>
             </div>
             <div class="dateChooser">
                 <h3>Please Select a date for your appointment</h3>
