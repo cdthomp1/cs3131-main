c53b6a96642e8f76c87296d549b2c1bd9faf5392
cdthomp1 <tho17015@byui.edu>
2020-02-20 22:05:42 -0700

    aasdas

--- projects/mint-auto-repair/user.php
+++ projects/mint-auto-repair/user.php
@@ -6,6 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa:500&display=swap" rel="stylesheet">
     
     <link rel="stylesheet" href="styles/main.css">
     <link rel="stylesheet" href="styles/user.css">
