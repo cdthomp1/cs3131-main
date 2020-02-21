aa5d1c7d26fffa34d4253386ece9d02e537812b1
cdthomp1 <tho17015@byui.edu>
2020-02-21 11:15:10 -0700

    test

--- projects/mint-auto-repair/scripts/schedule.js
+++ projects/mint-auto-repair/scripts/schedule.js
@@ -2,11 +2,21 @@ var currentID = '';
 var pastID = '';
 
 function showDate(id) {
+    checkIfPassed(id);
     changeColor(id);
     var dateField = document.getElementById("date");
     var date = id.replace(/([a-z])\w-+/g, "");
     dateField.value = date;
 
+}
+
+function checkIfPassed(id) {
+    var d = new Date();
+    var date = id.replace(/([a-z])\w-+/g, "");
+    var dateArr = date.split("-");
+    if (dateArr[2] < d.getDay - 1) {
+        alert ("Please Select a day that is after " + d.getDay - 1);
+    }
 }
 
 function changeColor(id) {
