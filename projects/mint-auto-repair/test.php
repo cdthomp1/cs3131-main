8e843a1023e8b891f8f91850453bff03657dc68e
cdthomp1 <tho17015@byui.edu>
2020-02-21 11:01:18 -0700

    tetst

--- projects/mint-auto-repair/test.php
+++ projects/mint-auto-repair/test.php
@@ -1,30 +1,46 @@
-<?php
-    session_start();
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<?php
-  $_SESSION["email"] = $_GET["email"];
-  $_SESSION["name"] = $_GET["name"];
-  $_SESSION["pass"] = $_GET["psw"];
-  $_SESSION["loggedIn"] = true;
-
-  if ($_GET["remember"] == "on") {
-      $_SESSION["rememberMe"] = true;
-  } else {
-    $_SESSION["rememberMe"] = false;
-  }
-
-?>
-
-</body>
-</html>
-
-
+<!DOCTYPE html> 
+<html> 
+      
+<head> 
+    <title> 
+        How to call PHP function 
+        on the click of a Button ? 
+    </title> 
+</head> 
+  
+<body style="text-align:center;"> 
+      
+    <h1 style="color:green;"> 
+        GeeksforGeeks 
+    </h1> 
+      
+    <h4> 
+        How to call PHP function 
+        on the click of a Button ? 
+    </h4> 
+      
+    <?php
+        if(array_key_exists('button1', $_POST)) { 
+            button1(); 
+        } 
+        else if(array_key_exists('button2', $_POST)) { 
+            button2(); 
+        } 
+        function button1() { 
+            echo "This is Button1 that is selected"; 
+        } 
+        function button2() { 
+            echo "This is Button2 that is selected"; 
+        } 
+    ?> 
+  
+    <form method="post"> 
+        <input type="submit" name="button1"
+                class="button" value="Button1" /> 
+          
+        <input type="submit" name="button2"
+                class="button" value="Button2" /> 
+    </form> 
+</head> 
+  
+</html> 
