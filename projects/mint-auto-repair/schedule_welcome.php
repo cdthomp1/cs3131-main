31ad2997678073adb6c4c2358ce47a231fee18a5
cdthomp1 <tho17015@byui.edu>
2020-02-21 22:03:23 -0700

    asd

--- projects/mint-auto-repair/schedule_welcome.php
+++ projects/mint-auto-repair/schedule_welcome.php
@@ -11,23 +11,27 @@
 
 <body>
     <?php include './navbar.php'; ?>
-    
+
     <h2>Welcome <?php echo $_SESSION["name"] ?><h3>Mint is excited to work with you and your vehicle. Lets begin.</h3>
 
-    <?php 
-    $sql = "SELECT * FROM vehicle WHERE customer_id='" . $_SESSION["userID"] . "'";
+        <?php
+        $sql = "SELECT * FROM vehicle WHERE customer_id='" . $_SESSION["userID"] . "'";
 
-    $sth = $db->query($sql);
-    $rows = $sth->rowCount();
-    
-    if ($rows >= 1) {
-        echo "what car are we doing today?";
-        foreach ($sth as $row) {
-            print_r($row);
+        $sth = $db->query($sql);
+        $rows = $sth->rowCount();
+
+        if ($rows >= 1) {
+            echo "what car are we doing today?";
+            echo '<form method="post" action="schedule_type.php">';
+            foreach ($sth as $row) {
+                echo '<input type="radio" id="'. $row['vehicle_make'] . $row['vehicle_model'] .'" name="cust_vehicle" value="' . $row['vehicle_make'] . $row['vehicle_model'] .'">';
+                echo '<label for="">' . $row['vehicle_make'] . ' ' . $row['vehicle_model'] . '</label>';
+            }
+            echo '<button type="submit" class="continue">Continue</button>';
+        } else {
+            echo "We need to add your car in our system.";
         }
-    } else {
-        echo "We need to add your car in our system.";
-    }
-    ?>
+        ?>
 </body>
+
 </html>
