24876a75a70fd45f5cae2f26f369530f3133f905
cdthomp1 <tho17015@byui.edu>
2020-02-21 23:03:59 -0700

    Test

--- projects/mint-auto-repair/schedule_time.php
+++ projects/mint-auto-repair/schedule_time.php
@@ -22,16 +22,26 @@
                 $result = $sth->fetchAll(PDO::FETCH_ASSOC);
 
                 $rows = $sth->rowCount();
+                $times = ['8:00 AM', '8:30 AM', '9:00 AM', '9:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '1:00 PM', '1:30 PM', '2:00 PM', '2:30 PM', '3:00 PM', '3:30 PM', '4:00 PM', '4:30 PM'];
+                $takenTime = [];
 
                 if ($rows > 1) {
                     echo "PASSED IF STATEMENT";
                     foreach($result as $res) {
-                        echo "<p>" . $res["appointment_time"] . "</p>";
+                        array_push($takenTime, $res["appointment_time"]);
                     }
                 } else {
                     echo "FAILED IF STATEMENT";
 
                     echo $result['appointment_date'];                    
+                }
+
+                foreach($times as $time) {
+                    if (in_array($time, $takenTime)) {
+                        echo "<p>This spot is taken</p>";
+                    } else {
+                        echo '<p>$time</p>';
+                    }
                 }
     ?>
 
