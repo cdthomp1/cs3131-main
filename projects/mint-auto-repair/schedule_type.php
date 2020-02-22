040b8001d344f2a3e0260073b3c2ea75765e39f1
cdthomp1 <tho17015@byui.edu>
2020-02-21 22:17:27 -0700

    asdf

--- projects/mint-auto-repair/schedule_type.php
+++ projects/mint-auto-repair/schedule_type.php
@@ -11,9 +11,21 @@
 
 <body>
     <?php include './navbar.php'; ?>
-    <?php 
-        $vehicle = urldecode($_POST["cust_vehicle"]);
-        echo '<h2>Okay '. $_SESSION["name"] . ', what does your ' . $vehicle . ' need?</h2>'; 
+    <?php
+    $vehicle = urldecode($_POST["cust_vehicle"]);
+    echo '<h2>Okay ' . $_SESSION["name"] . ', what does your ' . $vehicle . ' need?</h2>';
     ?>
+    <form method="post" action="schedule_date.php">
+        <label for="typeOf">What Type of Appointment do you need?</label><select id="typeOf" name="typeOf">
+            <option value="Oil Change">Oil Change</option>
+            <option value="Tire Rotaion">Tire Rotation</option>
+            <option value="Brakes">Brakes</option>
+            <option value="Alignment">Alignment</option>
+            <option value="Coolant Flush">Coolant Flush</option>
+            <option value="Wash">Wash</option>
+        </select>
+        <button type="submit" class="continue">Continue</button>
+    </form>
 </body>
+
 </html>
