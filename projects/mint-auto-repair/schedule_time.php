93588c7a2bc0b01e141a3ff35f988b227a2d9ece
cdthomp1 <tho17015@byui.edu>
2020-02-21 23:09:23 -0700

    Teast

--- projects/mint-auto-repair/schedule_time.php
+++ projects/mint-auto-repair/schedule_time.php
@@ -17,34 +17,38 @@
 
 
     $sql = "SELECT appointment_date, appointment_time FROM appointment where appointment_date = '" . $date . "'";
-                
-                $sth = $db->query($sql);
-                $result = $sth->fetchAll(PDO::FETCH_ASSOC);
 
-                $rows = $sth->rowCount();
-                $times = ['8:00 AM', '8:30 AM', '9:00 AM', '9:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '1:00 PM', '1:30 PM', '2:00 PM', '2:30 PM', '3:00 PM', '3:30 PM', '4:00 PM', '4:30 PM'];
-                $takenTime = [];
+    $sth = $db->query($sql);
+    $result = $sth->fetchAll(PDO::FETCH_ASSOC);
 
-                if ($rows > 1) {
-                    echo "PASSED IF STATEMENT";
-                    foreach($result as $res) {
-                        array_push($takenTime, $res["appointment_time"]);
-                    }
-                } else {
-                    echo "FAILED IF STATEMENT";
+    $rows = $sth->rowCount();
+    $times = ['8:00 AM', '8:30 AM', '9:00 AM', '9:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '1:00 PM', '1:30 PM', '2:00 PM', '2:30 PM', '3:00 PM', '3:30 PM', '4:00 PM', '4:30 PM'];
+    $takenTime = [];
 
-                    echo $result['appointment_date'];                    
-                }
+    if ($rows > 1) {
+        echo "PASSED IF STATEMENT";
+        foreach ($result as $res) {
+            array_push($takenTime, $res["appointment_time"]);
+        }
+    } else {
+        echo "FAILED IF STATEMENT";
 
-                foreach($times as $time) {
-                    if (in_array($time, $takenTime)) {
-                        echo "<p>This spot is taken</p>";
-                    } else {
-                        echo '<p>'.$time.'</p>';
-                    }
-                }
+        echo $result['appointment_date'];
+    }
     ?>
+    <form method="post" action="schedule_tech.php">
 
+        <?php
+        foreach ($times as $time) {
+            if (in_array($time, $takenTime)) {
+                echo "<p>This spot is taken</p>";
+            } else {
+                echo '<input type="radio" id="' . $time .'" name="time" value="'. $time .'">';
+                echo '<label for="time">'. $time .'</label><br />';
+            }
+        }
+        ?>
+    </form>
 </body>
 
 </html>
