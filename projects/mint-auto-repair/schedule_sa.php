17ecf0c88490abfa8e21728a9dbb39b27cc15d94
cdthomp1 <tho17015@byui.edu>
2020-02-21 23:33:58 -0700

    asdfgh

--- projects/mint-auto-repair/schedule_sa.php
+++ projects/mint-auto-repair/schedule_sa.php
@@ -13,14 +13,14 @@
     <?php include './navbar.php'; ?>
 
     <?php
-    $time = urldecode($_POST["time"]);
-    $_SESSION["time"] = $time;
+    $tech = urldecode($_POST["tech"]);
+    $_SESSION["tech"] = $tech;
 
     ?>
-    <form method="post" action="schedule_sa.php">
-        <label for="tech">Who would you like to work on your car? </label><select id="tech" name="tech">
+    <form method="post" action="schedule_confirm.php">
+        <label for="tech">Who would you like to be your Service Writer? </label><select id="tech" name="tech">
             <?php
-            foreach ($db->query("SELECT * FROM employees WHERE employee_position='Technician'") as $row) {
+            foreach ($db->query("SELECT * FROM employees WHERE employee_position='Service Writer'") as $row) {
                 echo '<option value="' . $row['employee_name'] . '">' . $row['employee_name'] . '</option>';
             }
             ?>
