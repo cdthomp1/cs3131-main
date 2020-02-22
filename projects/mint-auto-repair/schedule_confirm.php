abb6a17889c98a8332e3741aff0a12915f570e4d
cdthomp1 <tho17015@byui.edu>
2020-02-21 23:48:45 -0700

    sdfghj

--- projects/mint-auto-repair/schedule_confirm.php
+++ projects/mint-auto-repair/schedule_confirm.php
@@ -5,16 +5,19 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <link rel="stylesheet" href="styles/main.css">
+  <link href="https://fonts.googleapis.com/css?family=Comfortaa:500&display=swap" rel="stylesheet">
+
+  <link href="styles/main.css" type="text/css" rel="stylesheet" />
+  <link href="styles/calendar.css" type="text/css" rel="stylesheet" />
 
   <title>Document</title>
 </head>
 
 <body>
-<?php include './navbar.php'; ?>
+  <?php include './navbar.php'; ?>
   <?php
-      $sa = urldecode($_POST["sa"]);
-      $_SESSION["sa"] = $sa;
+  $sa = urldecode($_POST["sa"]);
+  $_SESSION["sa"] = $sa;
 
   $date = $_SESSION["date"];
   $name = $_SESSION["name"];
@@ -24,9 +27,11 @@
   $vehicle = $_SESSION["vehicle"];
   ?>
 
-  <h2>Congradulations <?php echo $name ?></h2>
-  <p>Here are your details about your appointment with <?php echo $tech ?> @ <?php echo $time ?> on <?php echo $date ?> for the <?php echo $typeOf ?> on the <?php echo $vehicle?>!</p>
-  <p>Your Service Writer is <?php echo $sa?>. Please arrive 30 minutes early to meet with them.</p>
+  <h2><?php echo $name ?></h2>
+  <p>Here are your details about your appointment with <?php echo $tech ?> @ <?php echo $time ?> on <?php echo $date ?> for the <?php echo $typeOf ?> on the <?php echo $vehicle ?>!</p>
+  <p>Your Service Writer is <?php echo $sa ?>. Please arrive 30 minutes early to meet with them.</p>
+
+  <a href="schedule_create.php">Looks Great! Create my appointment!</a>
 </body>
 
 </html>
---
+++ projects/mint-auto-repair/schedule_create.php
@@ -0,0 +1,20 @@
+<?php session_start(); ?>
+<?php include './dbConnect.php'; ?>
+
+<?php
+
+
+$date = $_SESSION["date"];
+$name = $_SESSION["name"];
+$time = $_SESSION["time"];
+$tech = $_SESSION["tech"];
+$typeOf = $_SESSION["typeOf"];
+$vehicle = $_SESSION["vehicle"];
+
+$techIdSql = "SELECT employee_id FROM employees where employee_name ='" . $tech ."'";
+
+$sth = $db->query($sql);
+
+print_r($sth);
+
+?>
