43d82d5d1407c2deaae724d122f7a020c72e4e21
cdthomp1 <tho17015@byui.edu>
2020-02-21 23:23:38 -0700

    asdfg

--- projects/mint-auto-repair/schedule_tech.php
+++ projects/mint-auto-repair/schedule_tech.php
@@ -1,8 +1,27 @@
-<label for="tech">Who would you like to work on your car? </label><select id="tech" name="tech">
+<?php session_start(); ?>
+<?php include './dbConnect.php'; ?>
+<html>
+
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa:500&display=swap" rel="stylesheet">
+
+    <link href="styles/main.css" type="text/css" rel="stylesheet" />
+    <link href="styles/calendar.css" type="text/css" rel="stylesheet" />
+</head>
+
+<body>
+    <?php include './navbar.php'; ?>
+    <label for="tech">Who would you like to work on your car? </label><select id="tech" name="tech">
                 <?php
                 foreach ($db->query("SELECT * FROM employees WHERE employee_position='Technician'") as $row) {
                     echo '<option value="' . $row['employee_name'] . '">' . $row['employee_name'] . '</option>';
                 }
                 ?>
             </select><br />
+</body>
+</html>
+
+
+
+
 
