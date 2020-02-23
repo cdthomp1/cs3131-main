dcd0e473bb64a2ef0d1faf1320c9f7f95ce646ec
cdthomp1 <tho17015@byui.edu>
2020-02-22 22:24:14 -0700

    Test

--- projects/mint-auto-repair/index.php
+++ projects/mint-auto-repair/index.php
@@ -8,7 +8,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/css?family=Comfortaa:500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles/main.css">
-    <link rel="stylesheet" href="styles/maintwo.css">
     <style>
 
     </style>
