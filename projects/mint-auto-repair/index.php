1ae430d230fa85861f4cadd0ec71181b5cdf549f
cdthomp1 <tho17015@byui.edu>
2020-02-22 20:16:29 -0700

    fux

--- projects/mint-auto-repair/index.php
+++ projects/mint-auto-repair/index.php
@@ -15,47 +15,46 @@
 
 <body>
 
-    <div class="header">
-        <?php include './navbar.php'; ?>
-
-        <img class="headerImage" src="./images/header-image.jpg">
-    </div>
-
-    <div class="main">
-        <h1>What Makes Mint Different?</h1>
+    <div class="hero-image">
+    <?php include './navbar.php'; ?>
+        <div class="hero-text">
+            <h1 id="title">Welcome to Mint</h1>
+            <p>The freshest service around</p>
+        </div>
         <div class="infoBox">
             <div class="box">
-                <div class="boxheader">Service</div>
+                <div class="boxheader">01</div>
                 <div class="boxcontent">
-                    <p>Here at Mint Auto, we take care to service your cars with the help they need! We have an advanced diagnostics program that pin points the real issue. No more beating around the bush to find what is truely wrong!</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae provident voluptatibus,
+                        optio, sed hic nobis error magni ipsum reprehenderit voluptates laboriosam iusto eaque magnam
+                        vero harum, quo quibusdam quis? Accusamus!</p>
                 </div>
             </div>
             <div class="box">
-                <div class="boxheader">Knowledge</div>
+                <div class="boxheader">02</div>
                 <div class="boxcontent">
-                    <p>We train our employees to excell in their respective fields. Our Technicians have years of experience. Our Service Managers have taken many courses to know how to best serve you!</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae provident voluptatibus,
+                        optio, sed hic nobis error magni ipsum reprehenderit voluptates laboriosam iusto eaque magnam
+                        vero harum, quo quibusdam quis? Accusamus!</p>
                 </div>
             </div>
             <div class="box">
-                <div class="boxheader">Professional</div>
+                <div class="boxheader">03</div>
                 <div class="boxcontent">
-                    <p>Being professional is key. We take care that our employees will not present you with hidden fees or surprise repairs. What you set up with your service advisor is what you will get. We respect you!</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae provident voluptatibus,
+                        optio, sed hic nobis error magni ipsum reprehenderit voluptates laboriosam iusto eaque magnam
+                        vero harum, quo quibusdam quis? Accusamus!</p>
                 </div>
             </div>
         </div>
+    </div>
+
+    <div class="main">
         <div class="about">
             <div class="aboutImg"><img class="imgAbout" src="images/aboutImg.jpg"></div>
             <div class="aboutArticle">
-                <div class="aboutHeader">
-                    <h2>About Us</h2>
-                    <p>Hit the road in your dependable car that get you where you’re going with no worries. Mint Auto
-                        Repair
-                        has provided Scranton, PA residents with comprehensive auto repairs since 1997. For personalized
-                        service and repairs from the experts, now ready to serve your car with premium auto care. We can
-                        do
-                        everything from suspension repairs to diagnostics. Rely on us when you need any general auto
-                        service.</p>
-                </div>
+                <h2>About Us</h2>
+                <p>Hit the road in your dependable car that get you where you’re going with no worries. Mint Auto Repair has provided Scranton, PA residents with comprehensive auto repairs since 1997. For personalized service and repairs from the experts, now ready to serve your car with premium auto care. We can do everything from suspension repairs to diagnostics. Rely on us when you need any general auto service.</p>
             </div>
         </div>
         <div class="ourStaff">
@@ -73,60 +72,49 @@
                 }
                 ?>
             </div>
-        </div>
-
-        <div class="clientReviews">
-            <div class="clientReviewHeader">
-                <h2>Client Reviews</h2>
-            </div>
-            <div class="clientReviewContainer">
-                <div class="review">
-                    <div>
-                        <h2>Sammy Johnson</h2>
-                        <span style="color: rgb(191, 138, 3)">★★★★★</span>
-                        <p>We have had our cars fixed here several times. The price is always comparable and well
-                            done!
-                            This last time, they fixed my hudbands clutch, but after driving it home he noticed it
-                            still
-                            wasn’t 100%. They took the car and will fix it again, they warranty their services
-                            thankfully. A frustrating situation but the customer service was great! We will continue
-                            to
-                            use them for our car needs!</p>
-                    </div>
+            <div class="clientReviews">
+                <div class="clientReviewHeader">
+                    <h2>Client Reviews</h2>
                 </div>
-                <div class="review">
-                    <div>
-                        <h2>Sammy Johnson</h2>
-                        <span style="color: rgb(191, 138, 3)">★★★★★</span>
-                        <p>We have had our cars fixed here several times. The price is always comparable and well
-                            done!
-                            This last time, they fixed my hudbands clutch, but after driving it home he noticed it
-                            still
-                            wasn’t 100%. They took the car and will fix it again, they warranty their services
-                            thankfully. A frustrating situation but the customer service was great! We will continue
-                            to
-                            use them for our car needs!</p>
+                <div class="clientReviewContainer">
+                    <div class="review">
+                        <div>
+                            <h2>Sammy Johnson</h2>
+                            <span style="color: rgb(191, 138, 3)">★★★★★</span>
+                            <p>We have had our cars fixed here several times. The price is always comparable and well done!
+                                This last time, they fixed my hudbands clutch, but after driving it home he noticed it still
+                                wasn’t 100%. They took the car and will fix it again, they warranty their services
+                                thankfully. A frustrating situation but the customer service was great! We will continue to
+                                use them for our car needs!</p>
+                        </div>
                     </div>
-                </div>
-                <div class="review">
-                    <div>
-                        <h2>Sammy Johnson</h2>
-                        <span style="color: rgb(191, 138, 3)">★★★★★</span>
-                        <p>We have had our cars fixed here several times. The price is always comparable and well
-                            done!
-                            This last time, they fixed my hudbands clutch, but after driving it home he noticed it
-                            still
-                            wasn’t 100%. They took the car and will fix it again, they warranty their services
-                            thankfully. A frustrating situation but the customer service was great! We will continue
-                            to
-                            use them for our car needs!</p>
+                    <div class="review">
+                        <div>
+                            <h2>Sammy Johnson</h2>
+                            <span style="color: rgb(191, 138, 3)">★★★★★</span>
+                            <p>We have had our cars fixed here several times. The price is always comparable and well done!
+                                This last time, they fixed my hudbands clutch, but after driving it home he noticed it still
+                                wasn’t 100%. They took the car and will fix it again, they warranty their services
+                                thankfully. A frustrating situation but the customer service was great! We will continue to
+                                use them for our car needs!</p>
+                        </div>
+                    </div>
+                    <div class="review">
+                        <div>
+                            <h2>Sammy Johnson</h2>
+                            <span style="color: rgb(191, 138, 3)">★★★★★</span>
+                            <p>We have had our cars fixed here several times. The price is always comparable and well done!
+                                This last time, they fixed my hudbands clutch, but after driving it home he noticed it still
+                                wasn’t 100%. They took the car and will fix it again, they warranty their services
+                                thankfully. A frustrating situation but the customer service was great! We will continue to
+                                use them for our car needs!</p>
+                        </div>
                     </div>
                 </div>
             </div>
+            <?php include './footer.php'; ?>
         </div>
     </div>
-
-    <?php include './footer.php'; ?>
 
     <script src="scripts/script.js"></script>
 </body>
